b0cc3d79ed06fd32d62b134ce159b9fe7fdf229b
Corning Sun <corningsun@163.com>
2022-10-18 14:09:36 +0800

    bak

---
+++ src/test/java/com/java/test/SortedListTest.java
@@ -0,0 +1,29 @@
+package com.java.test;
+
+import javafx.collections.transformation.SortedList;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.PriorityBlockingQueue;
+
+public class SortedListTest {
+
+    @Test
+    public void test() {
+
+        LinkedList<Integer> list = new LinkedList<>();
+
+        list.add(2);
+        list.add(1);
+        list.add(3);
+        list.add(10);
+
+        list.get(1);
+
+
+
+    }
+
+}
